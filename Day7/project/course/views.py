02f695d8d832765bb4a1f237af78cd31863a2b54
keyur9874 <73292698+keyur9874@users.noreply.github.com>
2021-06-06 18:00:43 +0530

    Django Basic Website

---
+++ Day7/project/course/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request,'course/home.html')
+
+def about(request):
+    return render(request,'course/about.html')
+
+def contact(request):
+    return render(request,'course/contact.html')
